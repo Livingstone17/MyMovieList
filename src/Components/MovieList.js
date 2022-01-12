f1c637746dc16333e33309e9030f489b54bdf380
Livingstone17 <peterson.omobolaji@gmail.com>
2022-01-12 14:26:56 +0100

    added search functionality to the app

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -5,8 +5,10 @@ import axios from "axios";
 const MovieList = ()=> {
 
     const [movies,setMovies] = useState([]);
+    const [query,setQuery] = useState('');
 
     useEffect(() => {
+        
         const key = 'c85933bac8b8a256dd48a7b1573cea01';
 
     const getMovieData = async()=>{
@@ -17,12 +19,25 @@ const MovieList = ()=> {
     getMovieData();
        
     }, [])
+    
+    const SearchMovies = async (e) =>{
+        e.preventDefault();
+        alert('search movie');
+        const key = 'c85933bac8b8a256dd48a7b1573cea01';
+        const res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`); 
 
+        setMovies(res.data.results);
+        console.log(res.data.results);
+    } 
     
     return (
         <div>
             <div className='container'>
                 <h5>Trending Movies Today</h5>
+                <form className='form' onSubmit={SearchMovies}>
+                    <input className='input' type='text' name='query' value={query} onChange={(e)=> setQuery(e.target.value)} placeholder='search movie by name'/>
+                    <button className='button' type='submit'>Search</button>
+                </form>
                 <div className='row'>
                 {movies.map((movie)=>(<MovieItem key={movie.id} movie={movie}/>))
                 } 
