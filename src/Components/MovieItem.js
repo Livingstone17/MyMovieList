f17c2efb55c85bb0c1ea40fe4e03662e5e78186c
Livingstone17 <peterson.omobolaji@gmail.com>
2022-01-12 09:50:41 +0100

    set up the app and fetching data

---
+++ src/Components/MovieItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function MovieItem({movie}) {
+
+    const baseUrl = "https://image.tmdb.org/t/p/w500/";
+
+    return (
+        <div className='card col-lg-3 col-md-3 moviecard'>
+            <div className='card-body'>
+                <img src={`${baseUrl}${movie.poster_path}`} className="img-fluid"/>
+                <div className='rating-container'>
+                    <h5 className='rating'>{movie.vote_average}</h5>
+                </div>
+               
+            </div>
+            
+        </div>
+    )
+}
+
+export default MovieItem
---
+++ src/Components/MovieList.js
@@ -0,0 +1,34 @@
+import React, {useState,useEffect} from 'react'
+import MovieItem from './MovieItem';
+import axios from "axios";
+
+const MovieList = ()=> {
+
+    const [movies,setMovies] = useState([]);
+
+    useEffect(() => {
+        const key = 'c85933bac8b8a256dd48a7b1573cea01';
+
+    const getMovieData = async()=>{
+        const res = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${key}`); 
+
+        setMovies(res.data.results);
+    };
+    getMovieData();
+       
+    }, [])
+
+    
+    return (
+        <div>
+            <div className='container'>
+                <div className='row'>
+                {movies.map((movie)=>(<MovieItem key={movie.id} movie={movie}/>))
+                } 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MovieList
